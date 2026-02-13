07cac6e40d8a768eff5cb6de10d3581632f93293
Oswaldo Villarreal <oswaldovillarreal@Oswaldos-MacBook-Air.local>
2026-02-13 17:17:32 -0500

    Projecto con la estructura de Python y el menu base

---
+++ src/main.py
@@ -0,0 +1,16 @@
+def mostrar_menu():
+    print("\n--- Gestor de Tareas ---")
+    print("1. Agregar tarea")
+    print("2. Listar tareas")
+    print("3. Marcar tarea como completada")
+    print("4. Eliminar tarea")
+    print("5. Buscar tarea")
+    print("0. Salir")
+
+
+def main():
+    print("Proyecto Autónomo 2 - Lógica de Programación en Python")
+
+
+if __name__ == "__main__":
+    main()
